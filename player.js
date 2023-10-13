900a1b126140933e766220146babae8847f969c8
shawn <shawnkan28@gmail.com>
2023-10-13 22:50:23 +0800

    adjust speed of player

--- player.js
+++ player.js
@@ -5,10 +5,14 @@ export class Player {
         this.height = 100;
         this.x = 0;
         this.y = this.game.height - this.height;
+        this.speed = 0
+        this.maxSpeed = 10;
     }
     update(input){
-        if(input.includes('ArrowRight')) this.x++;
-        else if(input.includes('ArrowLeft')) this.x--;
+        this.x += this.speed;
+        if(input.includes('ArrowRight')) this.speed = this.maxSpeed;
+        else if(input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
+        else this.speed = 0;
     }
     draw(context){
         context.fillRect(this.x, this.y, this.width, this.height);
