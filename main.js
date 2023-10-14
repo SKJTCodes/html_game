e610f86ff609ded08704d629d035ac550b81f6c1
shawn <shawnkan28@gmail.com>
2023-10-14 15:34:48 +0800

    able to view all player states

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+import { shadow_dog_coords } from "./assets/shadow_dog.js";
+
+let playerState = 'idle';
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change', function(e){
+    playerState = e.target.value;
+})
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
@@ -9,21 +17,22 @@ const playerImage = new Image();
 playerImage.src = './assets/shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
+
 let frameX = 0;
-let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
 
 function animate(){
+    const sprite_coords = shadow_dog_coords[playerState][frameX];
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.drawImage(playerImage, 
-        frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 
+        sprite_coords['x'], sprite_coords['y'], sprite_coords['width'], sprite_coords['height'], 
         0, 0, spriteWidth, spriteHeight
     );
-    
+   
     // every 5th gameFrame, will load 1 sprite from sheet
     if(gameFrame % staggerFrames == 0){
-        if(frameX < 12) frameX++;
+        if(frameX < shadow_dog_coords[playerState].length - 1) frameX++;
         else frameX = 0;
     }
     gameFrame++;
