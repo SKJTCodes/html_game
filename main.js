ff8c2457421f80df3d8aadc3c65aa7cc09ecd02a
shawn <shawnkan28@gmail.com>
2023-10-15 10:23:17 +0800

    Add jump sprite animation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { Sprite } from "./sprite.js";
+import { coords } from "./assets/leona/leona.coords.js";
 
 // let playerState = 'idle';
 // const dropdown = document.getElementById('animations');
@@ -6,25 +7,33 @@ import { Sprite } from "./sprite.js";
 //     playerState = e.target.value;
 // })
 
+let frame = 10;
+const state = 'fj';
+const animations = {
+    'idle': ['idle-1', 'idle-2', 'idle-3', 'idle-4', 'idle-5', 'idle-6', 'idle-7', 'idle-8'],
+    'fj': ['forward-jump-1', 'forward-jump-2', 'forward-jump-3', 'forward-jump-4', 'forward-jump-5', 'forward-jump-6', 'forward-jump-7', 'forward-jump-8', 'forward-jump-9', 'forward-jump-10', 'forward-jump-11']
+}
+
+document.getElementById('prev').addEventListener('click', function(e){
+    if(frame < 1) frame = animations[state].length - 1;
+    else frame--; 
+})
+document.getElementById('next').addEventListener('click', function(e){
+    if(frame >= animations[state].length - 1) frame = 0;
+    else frame++;
+})
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 700;
-const CANVAS_HEIGHT = canvas.height = 700;
-
-const frames = new Map([
-    ['idle-1', [[0, 0, 575, 523], [286, 523]]], 
-    ['idle-2', [[575, 0, 575, 523], [286, 523]]], 
-    ['idle-3', [[1150, 0, 575, 523], [286, 523]]], 
-    ['idle-4', [[1725, 0, 575, 523], [286, 523]]], 
-    ['idle-5', [[2300, 0, 575, 523], [286, 523]]], 
-    ['idle-6', [[2875, 0, 575, 523], [286, 523]]], 
-    ['idle-7', [[3450, 0, 575, 523], [286, 523]]]
-]);
+const CANVAS_WIDTH = canvas.width = 400;
+const CANVAS_HEIGHT = canvas.height = 400;
 
 const player = new Sprite({
-    position: {x: 350, y: 350},
-    imageSrc: "./assets/shadow_dog/shadow_dog.png",
-    frames: frames
+    position: {x: 200, y: 200},
+    imageSrc: "./assets/leona/Leona-trans.png",
+    frames: coords,
+    animations: animations,
+    state: state
 })
 
 let prevTime = 0;
@@ -33,10 +42,13 @@ let secPassed = 0;
 function animate(){
     // init
     requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillStyle = 'purple';
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    frame++;
 
     // Items to Loop through
-    player.update(ctx);
+    player.update(ctx, frame);
 }
 
 animate();
--- sprite.js
+++ sprite.js
@@ -1,19 +1,20 @@
 export class Sprite {
-    constructor({position, imageSrc, frames}){
+    constructor({position, imageSrc, frames, curFrame = 1, animations, state = 'idle'}){
         this.spriteSheet = new Image();
         this.spriteSheet.src = imageSrc;
         this.position = position;
         this.frames = frames;
-        this.curFrame = 1;
+        this.curFrame = curFrame;
+        this.animations = animations;
+        this.state = state;
     }
 
     draw(context){
-        const [[x, y, width, height], [originX, originY]] = this.frames.get(`idle-${this.curFrame}`);
+        const [[x, y, width, height], [originX, originY]] = this.frames.get(this.animations[this.state][this.curFrame]);
         // this is basically simulating an origin in a different area. in actual fact, the origin is still top left
         // but through using the magic of methethics, we can simulate that the center is the new origin.
         const nx = this.position.x - originX
         const ny = this.position.y - originY
-        console.log(nx, ny);
         context.drawImage(this.spriteSheet,
             // Position of cropping (x, y, width, height)
             x, y, width, height,
@@ -26,7 +27,7 @@ export class Sprite {
     drawDebug(context){
         context.lineWidth = 1;
         context.beginPath();
-        context.strokeStyle = 'red';
+        context.strokeStyle = 'white';
         context.moveTo(this.position.x - 5, this.position.y);
         context.lineTo(this.position.x + 4, this.position.y);
         context.moveTo(this.position.x, this.position.y - 5);
@@ -34,9 +35,12 @@ export class Sprite {
         context.stroke();
     }
 
-    update(context){
+    update(context, frame){
+        // this.curFrame = frame;
         this.draw(context);
-        this.curFrame++;
-        if (this.curFrame > 7) this.curFrame = 1;
+        if(frame % 10 == 0){
+            this.curFrame++;
+            if (this.curFrame > 7) this.curFrame = 0;
+        }
     }
 }
