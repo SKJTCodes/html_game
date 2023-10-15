c0c59d0bf3f21d56a474e2ba40aa4bbd2a3f9c7a
shawn <shawnkan28@gmail.com>
2023-10-15 20:26:08 +0800

    update origin of all sprites

--- main.js
+++ main.js
@@ -31,32 +31,32 @@ const sprite = new Sprite({
         'walk-up-6': [[455, 528, 51, 48], [25, 30]],
         'walk-up-7': [[519, 527, 51, 49], [25, 30]],
         // walk left
-        'walk-left-0': [[64, 592, 48, 48], [31, 0]],
-        'walk-left-1': [[129, 591, 51, 49], [30, 0]],
-        'walk-left-2': [[199, 591, 54, 49], [24, 0]],
-        'walk-left-3': [[261, 591, 53, 49], [26, 0]],
-        'walk-left-4': [[327, 592, 53, 48], [24, 0]],
-        'walk-left-5': [[392, 591, 54, 49], [23, 0]],
-        'walk-left-6': [[452, 591, 51, 49], [27, 0]],
-        'walk-left-7': [[516, 591, 48, 49], [27, 0]],
+        'walk-left-0': [[64, 592, 48, 48], [31, 29]],
+        'walk-left-1': [[129, 591, 51, 49], [30, 29]],
+        'walk-left-2': [[199, 591, 54, 49], [24, 29]],
+        'walk-left-3': [[261, 591, 53, 49], [26, 29]],
+        'walk-left-4': [[327, 592, 53, 48], [24, 29]],
+        'walk-left-5': [[392, 591, 54, 49], [23, 29]],
+        'walk-left-6': [[452, 591, 51, 49], [27, 29]],
+        'walk-left-7': [[516, 591, 48, 49], [27, 29]],
         // walk down
-        'walk-down-0': [[64, 655, 51, 49], [28, 0]],
-        'walk-down-1': [[128, 655, 51, 49], [28, 0]],
-        'walk-down-2': [[193, 656, 51, 48], [27, 0]],
-        'walk-down-3': [[256, 655, 51, 49], [28, 0]],
-        'walk-down-4': [[320, 655, 51, 49], [28, 0]],
-        'walk-down-5': [[384, 655, 51, 49], [28, 0]],
-        'walk-down-6': [[448, 656, 51, 48], [28, 0]],
-        'walk-down-7': [[512, 655, 51, 49], [28, 0]],
+        'walk-down-0': [[64, 655, 51, 49], [32, 31]],
+        'walk-down-1': [[128, 655, 51, 49], [32, 31]],
+        'walk-down-2': [[193, 656, 51, 48], [31, 31]],
+        'walk-down-3': [[256, 655, 51, 49], [32, 31]],
+        'walk-down-4': [[320, 655, 51, 49], [32, 31]],
+        'walk-down-5': [[384, 655, 51, 49], [32, 31]],
+        'walk-down-6': [[448, 656, 51, 48], [32, 31]],
+        'walk-down-7': [[512, 655, 51, 49], [32, 31]],
         // walk right
-        'walk-right-0': [[80, 720, 48, 48], [13, 0]],
-        'walk-right-1': [[140, 719, 51, 49], [17, 0]],
-        'walk-right-2': [[195, 719, 54, 49], [26, 0]],
-        'walk-right-3': [[262, 719, 53, 49], [23, 0]],
-        'walk-right-4': [[324, 720, 53, 48], [25, 0]],
-        'walk-right-5': [[386, 719, 54, 49], [27, 0]],
-        'walk-right-6': [[457, 719, 51, 49], [20, 0]],
-        'walk-right-7': [[524, 719, 48, 49], [17, 0]],
+        'walk-right-0': [[80, 720, 48, 48], [21, 30]],
+        'walk-right-1': [[140, 719, 51, 49], [25, 30]],
+        'walk-right-2': [[195, 719, 54, 49], [34, 30]],
+        'walk-right-3': [[262, 719, 53, 49], [31, 30]],
+        'walk-right-4': [[324, 720, 53, 48], [33, 30]],
+        'walk-right-5': [[386, 719, 54, 49], [35, 30]],
+        'walk-right-6': [[457, 719, 51, 49], [28, 30]],
+        'walk-right-7': [[524, 719, 48, 49], [25, 30]],
         // attack up
         'attack-up-0': [[4, 272, 54, 48], [28, 30]],
         'attack-up-1': [[68, 273, 51, 47], [28, 30]],
@@ -70,7 +70,7 @@ const sprite = new Sprite({
         'idle-up': [[7, 527, 51, 49], [25, 29]],
         'idle-left': [[3, 591, 51, 49], [28, 29]],
         'idle-down': [[0, 655, 51, 49], [32, 30]],
-        'idle-right': [[10, 719, 51, 49], [22, 30]],
+        'idle-right': [[10, 719, 51, 49], [28, 30]],
     },
     animations: {
         'walkUp': ['walk-up-0', 'walk-up-1', 'walk-up-2', 'walk-up-3', 'walk-up-4', 'walk-up-5', 'walk-up-6', 'walk-up-7'],
@@ -92,7 +92,7 @@ function checkOrigin() {
     const xPos = 60;
     const padding = 20;
     sprites.forEach((s, i) => {
-        s.state = 'walkUp';
+        s.state = 'walkRight';
         s.position.x = padding + (i * xPos);
         s.animateIndex = i;
         s.draw(ctx);
@@ -106,24 +106,24 @@ function animate() {
 
     // object updates
     // Female Character Logic
-    // if (input.keys.includes('ArrowUp') && !input.keys.includes('ArrowDown')) sprite.state = 'walkUp';
-    // else if (input.keys.includes('ArrowDown') && !input.keys.includes('ArrowUp')) sprite.state = 'walkDown';
-    // else if (input.keys.includes('ArrowLeft') && !input.keys.includes('ArrowRight')) sprite.state = 'walkLeft';
-    // else if (input.keys.includes('ArrowRight') && !input.keys.includes('ArrowLeft')) sprite.state = 'walkRight';
-    // else { // when no keys are pressed but previous direction walked is used to define idle position
-    //     sprite.animateIndex = 0;
-    //     if (sprite.state == "walkUp") sprite.state = "idleUp";
-    //     else if (sprite.state == 'walkDown') sprite.state = "idleDown";
-    //     else if (sprite.state == 'walkLeft') sprite.state = 'idleLeft';
-    //     else if (sprite.state == 'walkRight') sprite.state = 'idleRight';
-    // }
-    // // character movement
-    // console.log(sprite.position.x);
-    // if (input.keys.includes('ArrowUp') && !input.keys.includes('ArrowDown') && sprite.position.y >= 0) sprite.position.y -= VELOCITY;
-    // else if (input.keys.includes('ArrowDown') && !input.keys.includes('ArrowUp') && sprite.position.y + sprite.height <= canvas.height) sprite.position.y += VELOCITY;
-    // if (input.keys.includes('ArrowLeft') && !input.keys.includes('ArrowRight') && sprite.position.x - 15 >= 0) sprite.position.x -= VELOCITY;
-    // else if (input.keys.includes('ArrowRight') && !input.keys.includes('ArrowLeft') && sprite.position.x + 15 <= canvas.width) sprite.position.x += VELOCITY;
-    sprite.state = 'walkUp';
+    if (input.keys.includes('ArrowUp') && !input.keys.includes('ArrowDown')) sprite.state = 'walkUp';
+    else if (input.keys.includes('ArrowDown') && !input.keys.includes('ArrowUp')) sprite.state = 'walkDown';
+    else if (input.keys.includes('ArrowLeft') && !input.keys.includes('ArrowRight')) sprite.state = 'walkLeft';
+    else if (input.keys.includes('ArrowRight') && !input.keys.includes('ArrowLeft')) sprite.state = 'walkRight';
+    else { // when no keys are pressed but previous direction walked is used to define idle position
+        sprite.animateIndex = 0;
+        if (sprite.state == "walkUp") sprite.state = "idleUp";
+        else if (sprite.state == 'walkDown') sprite.state = "idleDown";
+        else if (sprite.state == 'walkLeft') sprite.state = 'idleLeft';
+        else if (sprite.state == 'walkRight') sprite.state = 'idleRight';
+    }
+    // character movement
+    console.log(sprite.position.x);
+    if (input.keys.includes('ArrowUp') && !input.keys.includes('ArrowDown') && sprite.position.y >= 0) sprite.position.y -= VELOCITY;
+    else if (input.keys.includes('ArrowDown') && !input.keys.includes('ArrowUp') && sprite.position.y + sprite.height <= canvas.height) sprite.position.y += VELOCITY;
+    if (input.keys.includes('ArrowLeft') && !input.keys.includes('ArrowRight') && sprite.position.x - 15 >= 0) sprite.position.x -= VELOCITY;
+    else if (input.keys.includes('ArrowRight') && !input.keys.includes('ArrowLeft') && sprite.position.x + 15 <= canvas.width) sprite.position.x += VELOCITY;
+    // sprite.state = 'idleUp';
     sprite.update(ctx);
     checkOrigin();
 }
